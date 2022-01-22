74b28aa3416da94da43166ea28ecb20280f7b340
DominikKeller <dojnik44@gmail.com>
2022-01-22 11:34:26 +0100

    Zadanie 2

--- src/com/company/Car.java
+++ src/com/company/Car.java
@@ -4,18 +4,21 @@ public class Car {
 
     final String producer;
     final String model;
-    final Integer YearOfProduction;
+    final String YearOfProduction;
     String colour;
-    Double engineVolume;
-    String Plates;
+    String plates;
+    String owner;
 
-    public Car(String producer, String model, Integer yearOfProduction) {
+    public Car(String producer, String model, String yearOfProduction, String colour, String plates,String owner){
         this.producer = producer;
         this.model = model;
         YearOfProduction = yearOfProduction;
+        this.colour = colour;
+        this.plates = plates;
+        this.owner = owner;
     }
     public String toString(){
-        return "Producent: " + this.producer+ " model: "+ this.model + " rok produkcji: "+ this.YearOfProduction;
+        return "Producent: " + this.producer+ " model: "+ this.model + " rok produkcji: "+ this.YearOfProduction+ "Kolor: " + this.colour + "Tablice rejestracyjne: "+this.plates;
     }
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,7 +6,8 @@ public class Main {
 
         Animal dog = new Animal("dog");
 
-        Car BMW = new Car("BMW","SERIE 3",2008);
+        Car BMW = new Car("BMW","Serie 3", "2008", "Grey","GLE NM44", "Dominik Keller");
+        Car BMW2 = new Car("BMW","Serie 3", "2014", "Black","GLE UA44", "Dominik Keller");
         Phone Iphone = new Phone("Apple","7", 2019,6.7,64);
 
 
@@ -20,7 +21,19 @@ public class Main {
         System.out.println(BMW.model);
         System.out.println(BMW.producer);
         System.out.println(BMW.YearOfProduction);
-        System.out.println(BMW);
+        System.out.println(BMW.colour);
+        System.out.println(BMW.plates);
+        System.out.println(BMW.owner);
+
+        System.out.println(BMW2.model);
+        System.out.println(BMW2.producer);
+        System.out.println(BMW2.YearOfProduction);
+        System.out.println(BMW2.colour);
+        System.out.println(BMW2.plates);
+        System.out.println(BMW2.owner);
+
+
         System.out.println(Iphone.getRamSize());
+
     }
 }
